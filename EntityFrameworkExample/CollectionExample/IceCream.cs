849876e5c3c7413e330c485e6feddcb66c203a77
Annie Decker <anniedecker@ou.edu>
2019-09-18 11:42:40 -0500

    In Class 9.18.19

---
+++ EntityFrameworkExample/CollectionExample/IceCream.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace CollectionExample
+{
+    public class IceCream
+    {
+        public string Size { get; set; }
+        public int Scoops { get; set; }
+
+        public IceCream()
+        {
+
+            Size = String.Empty;
+            Scoops = 0;
+              
+        }
+    }
+}
---
+++ EntityFrameworkExample/CollectionExample/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CollectionExample
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        { 
+          var icecreams = new List<IceCream>
+            {
+                new IceCream() { Size="Small", Scoops = 1},
+                new IceCream() { Size="Medium", Scoops =2},
+                new IceCream() { Size="Large", Scoops =3},
+                new IceCream() { Size="Double Decker", Scoops = 4}
+            };
+
+            foreach (IceCream iceCream in icecreams)
+            {
+                Console.WriteLine("A " + iceCream.Size + " has " + iceCream.Scoops + " scoops");
+            }
+        }
+    }
+}      
