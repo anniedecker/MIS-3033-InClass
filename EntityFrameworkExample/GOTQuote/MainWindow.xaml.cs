d489d477524877f95b534965b9c2a5a643777d3b
Annie Decker <anniedecker@ou.edu>
2019-09-29 09:54:27 -0500

    in class 9.25

---
+++ EntityFrameworkExample/GOTQuote/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Net.Http;
+using Newtonsoft.Json;
+
+namespace GOTQuote
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            //https://got-quotes.herokuapp.com/quotes
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = client.GetAsync(@"https://got-quotes.herokuapp.com/quotes").Result;
+                if (response.IsSuccessStatusCode)
+                {
+                    var content = response.Content.ReadAsStringAsync().Result;
+                    GOTQuote gq = JsonConvert.DeserializeObject<GOTQuote>(content);
+
+                    txtQuote.Text = gq.Quote + "\n" + " - " + gq.Character;
+                }
+            }
+        }
+    }
+}
